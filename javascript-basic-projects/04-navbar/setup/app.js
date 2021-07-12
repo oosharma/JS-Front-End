7b279de7797147f25f8e1887e4634ad3b042db08
Abhishek Sharma <abhisheksharma@abhisheks-mbp-2.lan>
2021-07-12 15:48:58 -0700

    update.

--- javascript-basic-projects/04-navbar/setup/app.js
+++ javascript-basic-projects/04-navbar/setup/app.js
@@ -3,3 +3,17 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+let toggleButton = document.querySelector(".nav-toggle");
+let linksListElement = document.querySelector(".links");
+
+toggleButton.addEventListener('click', function(){
+    let linksListElementClasses = linksListElement.classList;
+    if(linksListElementClasses.contains("toggled-link")){
+        linksListElementClasses.remove("toggled-link")
+    } else {
+        linksListElement.classList.add("toggled-link")
+    }
+    
+})
+
